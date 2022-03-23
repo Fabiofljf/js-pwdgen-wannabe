ef26e50f056eda56a0e6266b490815b8600216b2
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-23 15:47:34 +0100

    add js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Chiedere all'utente il suo nome
+const your_name = prompt("What is your name")
+console.log(your_name)
+
+// Chiedere all'utente il suo cognome
+const your_lastname = prompt("What is your last name")
+console.log(your_lastname)
+
+// Chiedere all'utente il suo colore preferito
+const your_color = prompt("What is your prefer color")
+console.log(your_color)
+
+// Scrivere sulla pagina: fabiopacifirosso22
+
+// scrivo il nome
+const username = "fabio"
+console.log(username)
+
+const userlastname = "pacifi"
+console.log(userlastname)
+
+const color = "rosso"
+console.log(color)
+
+const number = "22"
+console.log(number)
+
+
+document.getElementById('view').innerHTML = "fabio" + "pacifi " + "rosso" + "22"
